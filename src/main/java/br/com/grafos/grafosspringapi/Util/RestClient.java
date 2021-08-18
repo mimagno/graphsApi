401311337861e03db042572ad2dd6978a8c69c4c
MiMagno <magnoaraujob99@hotmail.com>
2021-08-18 00:33:31 -0300

    removed unused methods from RestClient class

--- src/main/java/br/com/grafos/grafosspringapi/Util/RestClient.java
+++ src/main/java/br/com/grafos/grafosspringapi/Util/RestClient.java
@@ -3,7 +3,6 @@ package br.com.grafos.grafosspringapi.Util;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +11,6 @@ public class RestClient {
 	private String server = "http://xtr-verde.consiste.com.br:9200/";
 	private RestTemplate rest;
 	private HttpHeaders headers;
-	private HttpStatus status;
 
 	public RestClient() {
 		this.rest = new RestTemplate();
@@ -30,45 +28,11 @@ public class RestClient {
 		headers.add("Accept", "*/*");
 	}
 	
-	public ResponseEntity<?> get(String uri) {
-	    HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-	    ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.GET, requestEntity, String.class);
-	    this.setStatus(responseEntity.getStatusCode());
-	    return responseEntity;
-	  }
-	
-	public ResponseEntity<?> get(String uri, String query) {
-	    HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-	    ResponseEntity<String> responseEntity = rest.getForEntity(server + uri, String.class, query);
-	    this.setStatus(responseEntity.getStatusCode());
-	    return responseEntity;
-	  } 
-
 	  public ResponseEntity<?> post(String uri, String json) {   
 	    HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
 	    ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.POST, requestEntity, String.class);
-	    this.setStatus(responseEntity.getStatusCode());
 	    return responseEntity;
 	  }
 
-	  public void put(String uri, String json) {
-	    HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
-	    ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.PUT, requestEntity, String.class);
-	    this.setStatus(responseEntity.getStatusCode());    
-	  }
-
-	  public void delete(String uri) {
-	    HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-	    ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.DELETE, requestEntity, String.class);
-	    this.setStatus(responseEntity.getStatusCode());
-	  }
-
-	  public HttpStatus getStatus() {
-	    return status;
-	  }
-
-	  public void setStatus(HttpStatus status) {
-	    this.status = status;
-	  } 
 }
 
